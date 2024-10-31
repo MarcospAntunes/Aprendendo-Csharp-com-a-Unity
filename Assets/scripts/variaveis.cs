83034f6ec56e591e8fa23b958cb5e86a7c6652d7
Marcospantunes <marcospantunes203@gmail.com>
2024-10-31 16:57:19 -0300

    feat: aprendendo a criar funções e a diferença entre Debug.Log e print

--- Assets/scripts/variaveis.cs
+++ Assets/scripts/variaveis.cs
@@ -3,23 +3,35 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class variaveis: MonoBehaviour {
-  int vidaPlayer = 10 , atackEnemy = 5;
-  float jumprForce = 5.5f;
-  bool mortePlayer = false;
-  string nomePlayer = "Marquitos";
+
+  int numero1, numero2;
 
   void Start() {
     // Esta função executa todas as instruções apenas uma vez no primeiro frame do jogo sem parar
 
-    ;
-
-    print(vidaPlayer);
-    nomePlayer = "Marcos";
-    string nomeEnemy = "Darth Vader";
+    numero1 = 10;
+    numero2 = 20;
+    Soma();
+    Multiplicacao(numero1, numero2);
   }
 
   void Update() { 
     // Esta função executa todas as instruções durante a execução do jogo sem parar
-    nomePlayer = "Fernanda";
   }
+
+  void Soma() {
+    int resultado;
+    resultado = numero1 + numero2;
+
+    Debug.Log(resultado); // Mesma coisa que o "print". Porém, o print só pode ser utilizado em classes do tipo MonoBehaviour, já o "Debug.Log" pode ser usado em qualquer lugar.
+
+  }
+
+  void Multiplicacao(int num1, int num2) {
+    int resultado = num1 * num2;
+
+    Debug.Log(resultado);
+  }
+
+
 }
