a8c4c13c50c3a3e4b840abc07c5f57217fa77792
Marcospantunes <marcospantunes203@gmail.com>
2024-11-03 14:40:44 -0300

    Métodos e variáveis static

---
+++ Assets/scripts/Statics.cs
@@ -0,0 +1,10 @@
+
+// Métodos static permitem que os acesse sem criar um objeto ou instânciar a classe.
+public class Statics {
+  public static bool mortePersonagem = false;
+  public static int Soma(int a, int b){
+    int resultado = a + b;
+    
+    return resultado;
+  }
+}
