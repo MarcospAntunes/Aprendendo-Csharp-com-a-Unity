3dfe1c297b0b986baeb73e8cd606a61dcfd09571
Marcospantunes <marcospantunes203@gmail.com>
2024-11-05 16:11:19 -0300

    Awake

---
+++ Assets/scripts/Funções da Unity/Awake.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class AwakeUnity: MonoBehaviour {
+  void Awake() {
+    // Executa antes do método Start.
+    // Serve para ativar um objeto que esta desativado na cena.
+  }
+
+  void Start() {
+
+  }
+}
